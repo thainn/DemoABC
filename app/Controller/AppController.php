27a254a7f30898a0c5beaa3d5c5d6a7a06be178e
Hai TM <haitm@leverages.jp>
2012-11-12 13:07:04 +0700

    ACL

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -38,7 +38,7 @@ class AppController extends Controller {
 		//Configure AuthComponent
 		$this->Auth->authorize = 'actions';
 		$this->Auth->loginAction = array('controller' => 'users', 'action' => 'login');
-		$this->Auth->logoutRedirect = array('controller' => 'users', 'action' => 'login');
-		$this->Auth->loginRedirect = array('controller' => 'posts', 'action' => 'add');
+// 		$this->Auth->logoutRedirect = array('controller' => 'users', 'action' => 'login');
+// 		$this->Auth->loginRedirect = array('controller' => 'index');
 	}
 }
